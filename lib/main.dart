e2aee5ea850748c5dc21624b7bcf61704c94a68e
nakao keisuke <r3grashopper193@gmail.com>
2024-07-07 07:45:34 +0900

    正方形が表示されるまで

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Row(
           mainAxisAlignment: MainAxisAlignment.center,
@@ -20,14 +20,26 @@ class MainApp extends StatelessWidget {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Expanded(
-                    child: FractionallySizedBox(
-                      widthFactor: 0.5,
-                      heightFactor: 0.5,
-                      alignment: FractionalOffset.center,
-                      child: Placeholder(),
-                    ),
-                  )
+                  LayoutBuilder(
+                    builder: (context, constraints) {
+                      final size = constraints.maxWidth;
+                      final sideSize = size * 0.5;
+                      return Container(
+                        width: sideSize,
+                        height: sideSize,
+                        color: Colors.blue,
+                        child: const Center(
+                          child: Text(
+                            'Square Box',
+                            style: TextStyle(color: Colors.white, fontSize: 24),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                  // Expanded(
+                  //   child:
+                  // ),
                 ],
               ),
             ),
